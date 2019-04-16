a7fc32e0baa49da17360493b596403ae6b2d521e
Ricky Ghrist <rghrist@onshift.com>
2019-04-16 16:07:00 -0400

    Updates

---
+++ precache-manifest.5ded1d2a798c42e4a714112d5814f92e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b162515c83c554475e0d",
+    "url": "/seasons/static/js/runtime~main.b162515c.js"
+  },
+  {
+    "revision": "f762e024821d06b61b7f",
+    "url": "/seasons/static/js/main.f762e024.chunk.js"
+  },
+  {
+    "revision": "619bf66c065dd1aac483",
+    "url": "/seasons/static/js/1.619bf66c.chunk.js"
+  },
+  {
+    "revision": "f762e024821d06b61b7f",
+    "url": "/seasons/static/css/main.64e2eddb.chunk.css"
+  },
+  {
+    "revision": "8c5362691b163ed26aa55ca7317d253b",
+    "url": "/seasons/index.html"
+  }
+];
